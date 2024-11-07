346335f2e4bcfbe05a9e9968387954aaf22d3428
Asnais-gh <e.greo75@gmail.com>
2024-11-07 18:27:54 +0200

    create login and register page ui

--- lib/view/log_in_page.dart
+++ lib/view/log_in_page.dart
@@ -8,92 +8,94 @@ import 'package:flutter/services.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
   static String id = 'LoginPage';
+
   @override
   Widget build(BuildContext context) {
+    // Get screen width and height for responsive adjustments
+    final screenHeight =
+        MediaQuery.of(context).size.height; // 890.2857142857143
+    final screenWidth = MediaQuery.of(context).size.width; // 411.42857142857144
+
     return Scaffold(
-      body: ListView(
+      body: Column(
         children: [
-          const SizedBox(
-            height: 100,
-          ),
-          Image.asset(
-            kLogo,
-            height: 300,
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'UK VOIP',
-                style: TextStyle(
-                    fontSize: 40, color: kColor, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              children: [
-                CustemFormTextField(
-                  hinttext: 'رقم هاتف',
-                  textInputType: TextInputType.number,
-                  textInputFormatter: <TextInputFormatter>[
-                    FilteringTextInputFormatter
-                        .digitsOnly, // Restricts input to numbers only
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                CustemFormTextField(
-                  hinttext: 'كلمة المرور',
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const CustomButten(buttenText: 'تسجيل الدخول'),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, RegisterPage.id);
-                      },
-                      child: const Text(
-                        '     حساب جديد',
-                        style: TextStyle(color: kColor),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: screenHeight * 0.1123234916559692),
+                  Image.asset(
+                    kLogo,
+                    height: screenHeight * 0.3369704749679076,
+                  ),
+                  SizedBox(height: screenHeight * 0.0561617458279846),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'UK VOIP',
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: kColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.0972222222222222),
+                    child: Column(
+                      children: [
+                        CustemFormTextField(
+                          hinttext: 'رقم هاتف',
+                          textInputType: TextInputType.number,
+                          textInputFormatter: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                        ),
+                        SizedBox(height: screenHeight * 0.0112323491655969),
+                        CustemFormTextField(
+                          hinttext: 'كلمة المرور',
+                          obscureText: true,
+                        ),
+                        SizedBox(height: screenHeight * 0.0224646983311938),
+                        const CustomButten(buttenText: 'تسجيل الدخول'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: screenHeight * 0.1348314607),
+                  Container(
+                    height: screenHeight * 0.0561617458279846,
+                    decoration: const BoxDecoration(
+                      color: kColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16),
                       ),
                     ),
-                    const Text(
-                      ' لا تملك حساب؟ يمكنك تسجيل ',
-                      style: TextStyle(color: Colors.black),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, RegisterPage.id);
+                          },
+                          child: const Text(
+                            '     حساب جديد',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        const Text(
+                          ' لا تملك حساب؟ يمكنك تسجيل ',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                        width: double.infinity,
-                        height: 50,
-                        decoration: const BoxDecoration(
-                          color: kColor,
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(16),
-                              topRight: Radius.circular(16)),
-                        )),
-                  ],
-                )
-              ],
+                  ),
+                ],
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
--- lib/view/register_page.dart
+++ lib/view/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:call_app/const.dart';
+import 'package:call_app/view/register_page.dart';
 import 'package:call_app/widgets/custom_butten.dart';
 import 'package:call_app/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
@@ -7,75 +8,89 @@ import 'package:flutter/services.dart';
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
   static String id = 'RegisterPage';
+
   @override
   Widget build(BuildContext context) {
+    // Get screen width and height for responsive adjustments
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: ListView(
+      body: Column(
         children: [
-          const SizedBox(
-            height: 100,
-          ),
-          Image.asset(
-            kLogo,
-            height: 300,
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'UK VOIP',
-                style: TextStyle(
-                    fontSize: 40, color: kColor, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              children: [
-                CustemFormTextField(
-                  hinttext: 'رقم هاتف',
-                  textInputType: TextInputType.number,
-                  textInputFormatter: <TextInputFormatter>[
-                    FilteringTextInputFormatter
-                        .digitsOnly, // Restricts input to numbers only
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                CustemFormTextField(
-                  hinttext: 'كلمة المرور',
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const CustomButten(buttenText: 'تسجيل حساب جديد'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'dont\'t have an acount?',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, RegisterPage.id);
-                      },
-                      child: const Text(
-                        '    Register',
-                        style: TextStyle(color: Color(0xffC7EDE6)),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: screenHeight * 0.1123234916559692),
+                  Image.asset(kLogo, height: screenHeight * 0.3369704749679076),
+                  SizedBox(height: screenHeight * 0.0561617458279846),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'UK VOIP',
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: kColor,
+                            fontWeight: FontWeight.bold),
                       ),
-                    )
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.0972222222222222),
+                    child: Column(
+                      children: [
+                        CustemFormTextField(
+                          hinttext: 'رقم هاتف',
+                          textInputType: TextInputType.number,
+                          textInputFormatter: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                        ),
+                        SizedBox(height: screenHeight * 0.0112323491655969),
+                        CustemFormTextField(
+                          hinttext: 'كلمة المرور',
+                          obscureText: true,
+                        ),
+                        SizedBox(height: screenHeight * 0.0224646983311938),
+                        const CustomButten(buttenText: 'تسجيل جديد'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: screenHeight * 0.1348314607),
+                  Container(
+                    height: screenHeight * 0.0561617458279846,
+                    decoration: const BoxDecoration(
+                      color: kColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text(
+                            '     هنا',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        const Text(
+                          '  تملك حساب؟ يمكنك تسجيل الدخول من  ',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
